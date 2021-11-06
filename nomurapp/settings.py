d6c336613072a5fcd25d3ba84d761cb4d2d2a830
kevin <kevinhananowahu@gmail.com>
2021-11-06 12:29:37 +0900

    なんかよくわからないが、動いた。

--- nomurapp/settings.py
+++ nomurapp/settings.py
@@ -75,10 +75,15 @@ WSGI_APPLICATION = 'nomurapp.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'postgres',
+        'USER': 'postgres',
+        'PASSWORD': 'postgres',
+        'HOST': 'db',
+        'PORT': 5432,
     }
 }
+
 
 
 # Password validation
